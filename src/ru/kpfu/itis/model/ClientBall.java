c073c7ddff45958435d7a19139b300ad6579b10c
LenaGavrilova <lena.gavrilova2408@mail.ru>
2022-12-22 21:37:50 +0300

    add entity ball

---
+++ src/ru/kpfu/itis/model/ClientBall.java
@@ -0,0 +1,37 @@
+package ru.kpfu.itis.model;
+
+import java.awt.*;
+
+public class ClientBall implements Ball {
+    private int x;
+    private int y;
+    private final int RADIUS = 5;
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.red);
+        graphics.fillOval(x - RADIUS, y - RADIUS, 2 * RADIUS, 2 * RADIUS);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
